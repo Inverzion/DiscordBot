8a78f9b2548ac2e90522d352d1bead3f73f205cd
Inverzion <82619267+Inverzion@users.noreply.github.com>
2021-04-15 23:22:36 -0700

    Update discord_bot_1.py

--- discord_bot_1.py
+++ discord_bot_1.py
@@ -10,11 +10,11 @@ async def on_message(message):
     bullying_list = {"yeetedon#1458", "Inverzion#1167"}
     import random
     bully = random.randint(0,2)
+    0 = ("You's a hoebag")
+    1 = ("haha, cunt")
+    2 = ("good one")
     
     if str(message.author) in bullying_list:
-        0 = ("You's a hoebag")
-        1 = ("haha, cunt")
-        2 = ("good one")
         await message.channel.send(bully)
 
     if str(message.channel) in channels and str(message.author) in valid_users:

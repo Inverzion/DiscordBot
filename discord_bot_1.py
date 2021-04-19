24eb2659a87fe30145fba48ad547f8740795c06f
Inverzion <82619267+Inverzion@users.noreply.github.com>
2021-04-19 11:36:23 -0700

    Update discord_bot_1.py

--- discord_bot_1.py
+++ discord_bot_1.py
@@ -44,6 +44,12 @@ async def on_message(message):
 
     if str(message.author) in bullying_list and luck == 1:
         await message.channel.send(random.choice(bully))
+    if message.content == "!help":
+        embed = discord.Embed(title = "Help on BOT", description = "Some neglected commands")
+        embed.add_field(name="!hello", value = "Bots description and greeting.")
+        embed.add_field(name="!users", value = "Shows the count of members.")
+        embed.add_field(name="!game", value = "Faulty programming bug")
+        await message.channel.send(content=None, embed=embed)
 
     if str(message.channel) in channels and str(message.author) in valid_users:
         if message.content.find("bazzas command") != -1:
@@ -52,8 +58,6 @@ async def on_message(message):
             await message.channel.send("Hey! I'm a self designed bot.")
         elif message.content.find("!game") != -1:
             await message.channel.send("...I'm not designed for that")
-        elif message.content.find("!help") != -1:
-            await message.channel.send("List of commands: (!hello, !hey, !game)")
         elif message.content =="!users":
             await message.channel.send(f"""# of Members: {id.member_count}""")
 

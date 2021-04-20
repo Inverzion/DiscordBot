1384e85a467a4adff88371c5ae1ec7a39b3b4299
Inverzion <82619267+Inverzion@users.noreply.github.com>
2021-04-20 12:26:40 -0500

    Update discord_bot_1.py

--- discord_bot_1.py
+++ discord_bot_1.py
@@ -38,7 +38,7 @@ async def on_message(message):
     import random
     bully = ("Honestly, fuck off.", "It's a beautiful day in the neighbourhood.", "haha, funny guy, haha...")
     luck = random.randint(0,2)
-    bullymemessage = ("You're so dumb, I bet you couldn't drink booze out of a boot if the isntructions were written on the sole.", f"""My god you're retarded, who let you out of the speds class?, was it{random.choice(valid_users)}?""", "That's it, whatever you just said provoked me to call the cops on you.", "Woah, that's not very nice, it's almost as offensive as the kids in my basement.", "Come to think of it, I've fucked both of your moms.", "Shit bro, if I knew you were single, I would've been clapping those cheeks.")
+    bullymemessage = ("You're so dumb, I bet you couldn't drink booze out of a boot if the isntructions were written on the sole.", "That's it, whatever you just said provoked me to call the cops on you.", "Woah, that's not very nice, it's almost as offensive as the kids in my basement.", "Come to think of it, I've fucked both of your moms.", "Shit bro, if I knew you were single, I would've been clapping those cheeks.")
 
     if str(message.author) in bullying_list and luck == 1:
         await message.channel.send(random.choice(bully))

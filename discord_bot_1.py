8676f0e8e89fee2774d0083894fc2fa31caefb74
Inverzion <82619267+Inverzion@users.noreply.github.com>
2021-04-21 01:47:42 -0700

    Update discord_bot_1.py

--- discord_bot_1.py
+++ discord_bot_1.py
@@ -8,12 +8,11 @@ client = discord.Client()
 @client.event
 async def on_message(message):
     id = client.get_guild(832113653758427146)
-    channels = {"testing-and-bot-commands"}
+    channels = {"newage-botcommands", "the-gate📱"}
     valid_users = {"Inverzion#1167", "ArtisticSloth#6223", "yeetedon#1458"}
     bullying_list = {"Inverzion#1167"}
     bully = ("Honestly, fuck off.", "It's a beautiful day in the neighbourhood.", "haha, funny guy, haha...")
     luck = random.randint(0,2)
-    bullymessage = ("You are so dumb, I bet you couldnt drink booze out of a boot if the isntructions were written on the sole.", "That is it, whatever you just said provoked me to call the cops on you.", "Woah, that is *not* very nice, it is almost as offensive as the kids in my basement.", "Come to think of it, I have fucked both of your moms.", "Shit bro, if I knew you were single, I would have been clapping those cheeks.")
 
     if str(message.author) in bullying_list and luck == 1:
         await message.channel.send(random.choice(bully))
@@ -22,9 +21,14 @@ async def on_message(message):
         embed.add_field(name="!hello", value = "Bots description and greeting.")
         embed.add_field(name="!users", value = "Shows the count of members.")
         embed.add_field(name="!game", value = "Faulty programming bug.")
+        embed.add_field(name="!bullyme", value = "Insulting yourself? really?")
+        embed.add_field(name="secret", value = "unknown")
         await message.channel.send(content=None, embed=embed)
 
     if str(message.channel) in channels and str(message.author) in valid_users:
+        if message.content.find("!bullyme") != -1:
+            bullymessage = (f"""{message.author}You are so dumb, I bet you couldnt drink booze out of a boot if the isntructions were written on the sole.""", f"""That is it, whatever {message.author} just said, provoked me to call the cops on you.""", f"""Woah, {message.author},that is *not* very nice, it is almost as offensive as the kids in my basement.""", f"""Come to think of it, I have fucked both of {message.author} moms.""", f"""Shit {message.author}, if I knew you were single, I would have been clapping those cheeks.""")
+            await message.channel.send(random.choice(bullymessage))
         if message.content.find("bazzas command") != -1:
             await message.channel.send("Oi, Aids Monkey. Bazza wanted me to give you this message. YOU'RE A FUCKING BITCH! *cough cough*")
         if message.content.find("!hello") != -1:

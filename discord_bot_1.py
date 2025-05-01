3559757ab571096607b3b264c83543cdf3e568c6
Inverzion <82619267+Inverzion@users.noreply.github.com>
2025-05-01 16:54:01 -0500

    Renewed discord_bot_1.py
    Some choice language was removed, edited, or modified in order to represent this bots dialogue options in a more concise way. No logical functions were edited and therefore the issue has not been fixed, however I believe Heroku might need to be renewed or I might have to switch the host.

--- discord_bot_1.py
+++ discord_bot_1.py
@@ -2,35 +2,35 @@ import discord
 import time
 import asyncio
 import random
-id = 832078861613006870
+id = |CLIENTID|
 client = discord.Client()
 
 @client.event
 async def on_message(message):
-    id = client.get_guild(832078861613006870)
-    channels = {"newage-botcommands", "the-gate📱"}
-    valid_users = {"Inverzion#1167", "ArtisticSloth#6223", "yeetedon#1458"}
-    bullying_list = {"Inverzion#1167"}
-    bully = ("Honestly, fuck off.", "It's a beautiful day in the neighbourhood.", "haha, funny guy, haha...")
+    id = client.get_guild(CLIENTID)
+    channels = {"|Channel1|", "|Channel2|"}
+    valid_users = {"User1", "User2", "User3"}
+    joking_list = {"User1"}
+    joke = ("Honestly, go away.", "It's a beautiful day in the neighbourhood.", "haha, funny guy, haha...")
     luck = random.randint(0,2)
 
-    if str(message.author) in bullying_list and luck == 1:
-        await message.channel.send(random.choice(bully))
+    if str(message.author) in joking_list and luck == 1:
+        await message.channel.send(random.choice(joke))
     if message.content == "!help":
-        embed = discord.Embed(title = "Help on BOT", description = "Some neglected commands")
+        embed = discord.Embed(title = "Help on BOT", description = "Some neglected commands.")
         embed.add_field(name="!hello", value = "Bots description and greeting.")
         embed.add_field(name="!users", value = "Shows the count of members.")
         embed.add_field(name="!game", value = "Faulty programming bug.")
-        embed.add_field(name="!bullyme", value = "Insulting yourself? really?")
+        embed.add_field(name="!joke", value = "Tells you a joke.")
         embed.add_field(name="secret", value = "unknown")
         await message.channel.send(content=None, embed=embed)
 
     if str(message.channel) in channels and str(message.author) in valid_users:
-        if message.content.find("!bullyme") != -1:
-            bullymessage = (f"""@{message.author} you are so dumb, I bet you couldnt drink booze out of a boot if the isntructions were written on the sole.""", f"""That is it, whatever @{message.author} just said, provoked me to call the cops on you.""", f"""Woah, @{message.author}, that is *not* very nice, it is almost as offensive as the kids in my basement.""", f"""Come to think of it, I have fucked both of @{message.author} moms.""", f"""Shit @{message.author}, if I knew you were single, I would have been clapping those cheeks.""")
-            await message.channel.send(random.choice(bullymessage))
+        if message.content.find("!joke") != -1:
+            jokemessage = (f"""@{message.author} oh, bless your heart.""", f"""Oh, why, hello @{message.author}, @{message.author} just asked me to say this.""", f"""Woah, @{message.author}, that is *not* very nice, does someone need to be muted?""", f"""Come to think of it, I haven't figured out to really exist as a bot yet.""", f"""@{message.author}, if I knew you were so funny, I would have been clapping these hands from the start.""")
+            await message.channel.send(random.choice(jokemessage))
         if message.content.find("bazzas command") != -1:
-            await message.channel.send("Oi, Aids Monkey. Bazza wanted me to give you this message. YOU'RE A FUCKING BITCH! *cough cough*")
+            await message.channel.send("Oi! |User{x}| wanted me to give you this message. YOU'RE A FUNNY GUY! *cough cough*")
         if message.content.find("!hello") != -1:
             await message.channel.send("Hey! I'm a self designed bot.")
         if message.content.find("!game") != -1:
@@ -38,4 +38,4 @@ async def on_message(message):
         if message.content.find("!users") != -1:
             await message.channel.send(f"""# of Members: {id.member_count}""")
 
-client.run("ODMyMDc4ODYxNjEzMDA2ODcw.YHej0w.a14W0YJkeKWcjzids74BvQyTsI4")
+client.run("[Numbers].[Numerics].[Server-ID]")
